08252548bbb385be3e4b8f9d56bd86b94b62e2de
Harshil Valiya <harshilvaliya40@gmail.com>
2025-07-09 15:14:45 +0530

    half

---
+++ slides.js
@@ -0,0 +1,29 @@
+const slides = [
+  {
+    title: "Desert Oasis Pool",
+    image: "./img1.png",
+    url: "https://www.youtube.com/",
+  },
+  {
+    title: "Mountain Landscape",
+    image: "./img2.png",
+    url: "https://www.youtube.com/",
+  },
+  {
+    title: "Coastal Sunset",
+    image: "./img3.png",
+    url: "https://www.youtube.com/",
+  },
+  {
+    title: "Forest Path",
+    image: "./img4.png",
+    url: "https://www.youtube.com/",
+  },
+  {
+    title: "Urban Skyline",
+    image: "./img5.png",
+    url: "https://www.youtube.com/",
+  },
+];
+
+export default slides;
